e048fbb53457411e0a5c5bd25c782c4b03ba3e3f
Carlos Tovar <carlos.mercury92@gmail.com>
2024-02-07 12:47:42 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+    This project is related with a web application
+    for Routiva.
+
+    Here I describe the way to add this project to GitHub.
+
+    - git init -b main
+    - git add .
+
+"""

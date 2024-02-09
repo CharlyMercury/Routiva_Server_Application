8555293c0cd869fc1ac90cc014c7deff57870ab7
Carlos Tovar <carlos.mercury92@gmail.com>
2024-02-09 15:26:23 -0600

    Example

---
+++ main_2.py
@@ -0,0 +1,19 @@
+"""
+    This project is related with a web application
+    for Routiva.
+
+    Here I describe the way create a git repository for the project.
+
+    - git init -b main
+    - git add .
+    - git commit -m "First commit"
+
+    Here I describe the way to add this project to GitHub.
+
+    - Create a new repository
+    - Copy the REMOTE-URL
+    - git remote add origin https://github.com/CharlyMercury/Routiva_Server_Application.git
+    - git remote -v
+    - git push origin main
+    - git pull origin main
+"""
